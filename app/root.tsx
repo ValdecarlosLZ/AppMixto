7a8b8216bb15089fc28c4026ff86c16cfa091d21
Valdecarlos Henrique Garcia dos Santos <04733453159@seduc.mt.gov.br>
2023-12-15 09:10:08 -0400

    .

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,18 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import { LiveReload } from "@remix-run/react";
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <title>Mixando o mundo</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        Bora brincar com isso<LiveReload />
       </body>
     </html>
   );
